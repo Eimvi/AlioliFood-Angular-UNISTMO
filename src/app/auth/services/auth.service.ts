30511bf2461dc5909496c3e3cff8986a7740247b
EdumanReyes <edumanreyes@gmail.com>
2022-05-09 21:02:17 -0500

    Agregado Login Service API

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {tap} from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Login, Tel } from '../interfaces/auth.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private readonly URL = environment.urlApi;                               //Obtenemos la API desde el enviroment
+  constructor(private http: HttpClient) { }
+
+  login(phone: Tel){                                                       //Nuestra función recibe un parametro de tipo Tel
+   return this.http.post<Login>(`${this.URL}auth/login`,phone).pipe(       //Hacemos la petición
+      tap((user)=>{
+        localStorage.setItem('token',user.body.user.token);                //Guardamos el token en el localStorage
+      })
+    );
+  }
 }
