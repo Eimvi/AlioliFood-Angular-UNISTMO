6466e8e95b3e701e3d32439ef9b50f6fe5c7674a
Cristopher <cristopher.emodesto@gmail.com>
2022-05-10 13:49:28 -0500

    Added redirect auth login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, { path: 'menu', loadChildren: () => import('./menu/menu.module').then(m => m.MenuModule) }];
+const routes: Routes = [
+  {
+    path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'menu', loadChildren: () => import('./menu/menu.module').then(m => m.MenuModule)
+  },
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
