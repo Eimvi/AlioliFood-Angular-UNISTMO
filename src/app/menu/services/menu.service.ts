81e07fafb2898866c18e8d16b94bc4f2a74a90cd
MarioBasilio08 <andres.basilio@outlook.es>
2022-05-10 00:15:40 -0500

    Agregue getFood

--- src/app/menu/services/menu.service.ts
+++ src/app/menu/services/menu.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Platillos } from '../interfaces/platillos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
+  private readonly URL =  environment.urlApi;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+
+  }
+
+  getFood() {
+    return this.http.get<Platillos>(`${this.URL}food`).pipe(
+      map(comida => {
+        console.log(comida)
+        return comida.body.foods
+      })
+    )
+  }
+
+
+
 }
