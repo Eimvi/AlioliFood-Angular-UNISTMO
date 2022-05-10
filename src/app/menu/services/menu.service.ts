47db6a7a7cd45cfbe9a5483e35f0892d92e9de63
Samuel200028 <lopezdiaz200028@gmail.com>
2022-05-10 00:14:10 -0600

    Creado funcion getCategory

--- src/app/menu/services/menu.service.ts
+++ src/app/menu/services/menu.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Platillos } from '../interfaces/platillos';
+
+//import { Categorias } from '../interfaces/categorias';
+import { Categoria } from '../interfaces/categoria';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
+  private readonly URL = 'https://alioli-food-api.herokuapp.com/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+
+  }
+
+  getFood() {
+    return this.http.get<Platillos>(`${this.URL}food`).pipe(
+      map(comida => {
+        console.log(comida)
+        return comida.body.foods
+      })
+    )
+  }
+
+  getCategory() {
+    return this.http.get<Categoria>(`${this.URL}category`).pipe(
+      map(comida => {
+        console.log(comida)
+        return comida.body.categories
+      })
+    )
+  }
+
 }
