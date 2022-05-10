77b20484dd46e8f3f9876946a0a151a9f5bef658
EdumanReyes <edumanreyes@gmail.com>
2022-05-10 13:42:58 -0500

    Agregado ReactiveFormsModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-  
+
     LoginComponent
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
