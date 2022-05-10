3144f2f74cf31ac487c9cfcdd329c4243d1e3430
JoseNolasco001 <puuxii.jn@gmail.com>
2022-05-10 10:18:01 -0500

    agregue funcion logout y verpedidos

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -21,6 +21,7 @@ export class MenuComponent implements OnInit {
 
   verpedidos(){
     this.menuService.guardarPlatillos(this.pedidoAlmuerzo);
+    this.router.navigateByUrl('verpedidos');
   }
 
 }
