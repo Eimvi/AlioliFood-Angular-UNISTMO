2acd10fc8a9e2fe87643c3b41d3a5520ded4b8e2
JoseNolasco001 <puuxii.jn@gmail.com>
2022-05-24 22:41:15 -0500

    agregado API google

--- src/app/ver-pedidos/service/ubicacion.service.ts
+++ src/app/ver-pedidos/service/ubicacion.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { RootObject, Result } from '../interfaces/direccion';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +14,11 @@ export class UbicacionService {
   lat!:number;
   lng!:number;
   bandera:boolean=false;
+  direccion!:string;
 
-  constructor() { }
+  dir!:Result[];
+
+  constructor(private http: HttpClient) { }
 
   getLocalizacion(){
     if (navigator.geolocation) {
@@ -20,10 +28,17 @@ export class UbicacionService {
           this.lng = position.coords.longitude;
           console.log(this.lat);
           console.log(this.lng);
+          this.getAdrres().subscribe(
+            resp=>{
+              this.dir=resp;
+              console.log(this.dir[0].formatted_address);
+              this.direccion=this.dir[0].formatted_address;
+              localStorage.setItem('dir',this.direccion);
+            }
+          );
           this.bandera=false;
         }
       },
-
         (error: any) => {this.bandera=true}
 
         );
@@ -32,4 +47,13 @@ export class UbicacionService {
     }
     return this.bandera;
   }
+
+  getAdrres(){
+    return this.http.post<RootObject>('https://alioli-food-api.herokuapp.com/order/get-location',{lat:this.lat,long:this.lng}).pipe(
+      map(resp =>{
+        return resp.results;
+      })
+    )
+  }
+
 }
