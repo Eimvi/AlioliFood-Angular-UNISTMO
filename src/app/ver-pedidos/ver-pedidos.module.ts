29b4e70cb3ca2fe0b0cd97f468f85100106c40aa
EdumanReyes <edumanreyes@gmail.com>
2022-05-25 16:12:32 -0500

    Eliminado PedidoComponent

--- src/app/ver-pedidos/ver-pedidos.module.ts
+++ src/app/ver-pedidos/ver-pedidos.module.ts
@@ -4,15 +4,13 @@ import { CommonModule } from '@angular/common';
 import { VerPedidosRoutingModule } from './ver-pedidos-routing.module';
 import { VerPedidosComponent } from './ver-pedidos.component';
 import { DireccionComponent } from './components/direccion/direccion.component';
-import { PedidoComponent } from './components/pedido/pedido.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     VerPedidosComponent,
-    DireccionComponent,
-    PedidoComponent
+    DireccionComponent
   ],
   imports: [
     CommonModule,
