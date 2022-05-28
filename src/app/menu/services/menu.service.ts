c923e64a19e4b4b0f9139b87234bca3c09b13b29
MarioBasilio08 <andres.basilio@outlook.es>
2022-05-27 22:50:01 -0500

    Funcion actualizacion pedido localstorage

--- src/app/menu/services/menu.service.ts
+++ src/app/menu/services/menu.service.ts
@@ -2,14 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { Platillos } from '../interfaces/platillos';
+import { Platillos, Pedido } from '../interfaces/platillos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
   private readonly URL =  environment.urlApi;
-
   constructor(private http: HttpClient) { }
 
   getFood() {
@@ -20,6 +19,14 @@ export class MenuService {
     )
   }
 
+  actualizarPedido(clave:string, valor:Pedido[]|number[]){
+    if(localStorage.getItem(clave) !== null){
+      localStorage.setItem(clave, JSON.stringify(valor))
+    }else{
+      localStorage.removeItem(clave);
+      localStorage.setItem(clave, JSON.stringify(valor))
+    }
+  }
 
 
 }
