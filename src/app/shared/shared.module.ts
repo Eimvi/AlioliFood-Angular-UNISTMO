fb0759f09be99cac52b48ffbc67738756f4d7cef
MarioBasilio08 <andres.basilio@outlook.es>
2022-06-03 10:42:13 -0500

    Import MenuRoutingModule para navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
+import { MenuRoutingModule } from '../menu/menu-routing.module';
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MenuRoutingModule
   ],
   exports: [NavbarComponent]
 })
